d3502bf98db735be92b329e7fb339281b99551f3
Admin-Dcb <Admin-Dcb@DESKTOP-UVSN45T>
2018-06-11 12:23:18 +0800

    inital java project

---
+++ helpertools/src/main/java/com/happywork/helpertools/App.java
@@ -0,0 +1,13 @@
+package com.happywork.helpertools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
